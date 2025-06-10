afb07fe454003d0cc9e267974358bf6b1c0ac970
mitarashi <mitarashimail2025@gmail.com>
2025-06-10 12:07:51 +0900

    アプリのタイトルを「ポケモン検索」から「ポケモンWordle検索ツール」に変更

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>ポケモン検索</h1>
+        <h1>ポケモンWordle検索ツール</h1>
         
         <div className="search-container">
           <div className="search-section">
